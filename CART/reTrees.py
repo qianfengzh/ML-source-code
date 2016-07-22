f2fd31a94c520cf0f6715b60fdc1a71c242f9901
Vincent <qianfeng_zh@163.com>
2016-07-22 17:29:08 +0800

    mr

--- CART/reTrees.py
+++ CART/reTrees.py
@@ -9,6 +9,7 @@
 
 
 import numpy as np
+from numpy import linalg as la
 
 def loadDataSet(fileName):
 	dataMat = []
@@ -42,26 +43,6 @@ def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
 	retTree['right'] = createTree(rSet,leafType, errType, ops)
 	return retTree
 
-<<<<<<< HEAD
-#------------------------------------------
-
-def regLeaf(dataSet):
-	return np.mean(dataSet[:,-1])
-
-def regErr(dataSet):
-	return np.var(dataSet[:,-1]) * np.shape(dataSet)[0]
-
-def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
-	tolS = ops[0]
-	tolN = ops[1]
-	if len(set(dataSet[:,-1].T.tolist()[0])) == 1: # label只有一个值
-		return None, leafType(dataSet)
-	m, n = np.shape(dataSet)
-	S = errType(dataSet)
-=======
-
-
-
 def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)): 
 	# ops控制 leaf node最小样本数（即进行划分的样本数控制）
 	tolS = ops[0] # 误差阈值
@@ -90,7 +71,112 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
 	if (np.shape(mat0)[0] < tolN) or (np.shape(mat1)[0] < tolN):
 		return None, leafType(dataSet) # 上面只是做了 continue,并没有返回值
 	return bestIndex, bestValue
->>>>>>> 0a572896f7379e85b195f03c1151b4c57c2b623d
+
+
+# =====================================
+# 对树进行后剪枝
+def isTree(obj):
+	return (type(obj).__name__=='dict')
+
+def getMean(tree):
+	if isTree(tree['right']):
+		tree['right'] = getMean(tree['right'])
+	if isTree(tree['left']):
+		tree['left'] = getMean(tree['left'])
+	return (tree['left'] + tree['right']) / 2.0
+
+def prune(tree, testData):
+	if np.shape(testData)[0] == 0: # 没有测试数据则对树进行塌陷处理
+		return getMean(tree)
+	if (isTree(tree['right']) or isTree(tree['left'])): # 切分数据集，寻找树叶
+		lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+	if isTree(tree['left']): # 左树递归
+		tree['left'] = prune(tree['left'], lSet)
+	if isTree(tree['right']): # 右树递归
+		tree['right'] = prune(tree['left'], lSet)
+	if not isTree(tree['left']) and not isTree(tree['right']): # 从叶节点的第一个父节点回溯
+		lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+		errorNoMerge = np.sum(np.power(lSet[:,-1] - tree['left'],2)) +\
+			np.sum(np.power(rSet[:,-1] - tree['right'],2))
+		treeMean = (tree['left'] + tree['right']) / 2.0
+		errorMerge = np.sum(np.power(testData[:,-1] - treeMean,2))
+		if errorMerge < errorNoMerge:
+			print 'merging'
+			return treeMean
+		else:
+			return tree
+	else:
+		return tree
+
+
+
+#================================================
+# 回归模型树
+
+def linearSolve(dataSet):
+	m,n = np.shape(dataSet)
+	X = np.mat(np.ones((m,n)))
+	Y = np.mat(np.ones((m,1)))
+	X[:,1:n] = dataSet[:,0:n-1]
+	Y = dataSet[:,-1]
+	xTx = X.T * X
+	if la.det(xTx) = 0.0:
+		raise NameError('This matrix is singular, cannot do inverse,\n\
+			try increasing the second value of ops.')
+	ws = xTx.I * (X.T * Y)
+	return ws, X, Y
+
+def modelLeaf(dataSet):
+	ws, X, Y = linearSolve(dataSet)
+	return ws
+
+def modelErr(dataSet):
+	ws, X, Y = linearSolve(dataSet)
+	yHat = X * ws
+	return np.sum(np.power(Y - yHat, 2))
+
+
+
+
+# ---======================
+# 树回归预测
+
+def regTreeEval(model, inDat):
+	return float(model)
+
+def modelTreeEval(model, inDat):
+	n = np.shape(inDat)[1]
+	X = np.mat(np.ones((1, n+1)))
+	X[:,1:n+1] = inDat
+	return flaot(X * model)
+
+def treeForecast(tree, inData, modelEval=regTreeEval):
+	if not isTree(tree):
+		return modelEval(tree, inData)
+	if inData[tree['spInd']] > tree['spVal']:
+		if isTree(tree['left']):
+			return treeForecast[tree['left'], inData, modelEval]
+		else:
+			return modelEval(tree['left'], inData)
+	else:
+		if isTree(Tree['right']):
+			return treeForecast[tree['right'], inData, modelEval]
+		else:
+			return modelEval(tree['right'], inData)
+
+def createForeCast(tree, testData, modelEval=regTreeEval):
+	m = len(testData)
+	yHat = np.mat(np.zeros((m,1)))
+	for i in range(m):
+		yHat[i,0] = treeForecast(tree, np.mat(testData[i]), modelEval)
+	return yHat
+
+
+
+
+
+
+
 
 
 
--- KMeans/kMeans.py
+++ KMeans/kMeans.py
@@ -18,24 +18,24 @@ def loadDataSet(fileName):
 		datMat.append(fltLine)
 	return datMat
 
-def disEclud(vecA, vecB):
-	return np.sqrt(sum(np.power(vecA - vecB), 2))
+def distEclud(vecA, vecB):
+	return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
 
 
 def randCent(dataSet, k):
 	n = np.shape(dataSet)[1]
 	centroids = np.mat(np.zeros((k,n)))
 	for j in range(n): # 随机初始化质心
-		minJ = min(dataSet[:,j])
-		rangeJ = float(max(dataSet[:,j]) - minJ)
-		centroids[:,j] = minJ + rangeJ * random.rand(k,1) # 每次初始化所有质心的一个特征列
+		minJ = np.min(dataSet[:,j])
+		rangeJ = float(np.max(dataSet[:,j]) - minJ)
+		centroids[:,j] = minJ + rangeJ * np.random.rand(k,1) # 每次初始化所有质心的一个特征列
 	return centroids
 
 #------------------------------------------
 
-def kMeans(dataSet, k, distMeas=disEclud, createCent=randCent):
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
 	m = np.shape(dataSet)[0]
-	clusterAssment = np.mat(np.zeros((m,2)))
+	clusterAssment = np.mat(np.zeros((m,2))) # 存放每个点的标签
 	centroids = createCent(dataSet, k)
 	clusterChanged = True
 	while clusterChanged: # 循环迭代更新质心，直到所有点的隶属不在变化
@@ -43,32 +43,59 @@ def kMeans(dataSet, k, distMeas=disEclud, createCent=randCent):
 		for i in range(m): # 对所有点计算与各质心距离，划分隶属
 			minDist = np.inf
 			minIndex = -1
-			for j in range(k):
+			for j in range(k): # 遍历质心，计算距离，确定点的隶属族
 				distJI = distMeas(centroids[j,:], dataSet[i,:])
-				if distJI = < minDist:
+				if distJI < minDist:
 					minDist = distJI
 					minIndex = j
 			if clusterAssment[i,0] != minIndex: # 有的点隶属变化了，需重新计算（因为质心与本族内所有点的计算有关系）
 				clusterChanged = True
 			clusterAssment[i,:] = minIndex, minDist**2
-		print centroids
+		# print centroids
 		for cent in range(k): # 更新质心
-			ptsInClust = dataSet[np.nonzero(clusterAssment[:,0].A==cent)[0]]
+			# 获取各族中点的索引
+			ptsInClust = dataSet[np.nonzero(clusterAssment[:,0].A==cent)[0]] # 使用索引比对
 			centroids[cent,:] = np.mean(ptsInClust, axis=0)
 	return centroids, clusterAssment
 
 
 # 二分K-Means
-def biKmeans(dataSet, k, disMeas=disEclud):
+def biKmeans(dataSet, k, distMeas=distEclud):
 	m = np.shape(dataSet)[0]
-	clusterAssment = np.mat(np.zeros(m,2))
-	centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+	clusterAssment = np.mat(np.zeros((m,2)))
+	centroid0 = np.mean(dataSet, axis=0).tolist()[0] # 创建初始单个族
 	centList = [centroid0]
-	for j in range(m): # 计算所有不点与族质心的距离
-		clusterAssment[j,1] = disMeas(np.mat(centroid0), dataSet[j,:])**2
-	while (len(centList) < k): # 为达到指定的族数 k
+	for j in range(m): # 计算所有点与族质心的距离
+		clusterAssment[j,1] = distMeas(np.mat(centroid0), dataSet[j,:])**2
+	while (len(centList) < k): # 是否达到指定的族数 k
 		lowestSSE = np.inf
-		for i in range(len(centList)):
-			ptsInClust = dataSet[np.nonzero(clusterAssment[:,0].A==1)[0],:]
+		for i in range(len(centList)): # 尝试划分每一族
+			ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:,0].A==i)[0],:] # 获取此族内所有样本
+			centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+			# 计算切分之后的 sse
+			sseSplit = np.sum(splitClustAss[:,1])
+			sseNotSplit = np.sum(clusterAssment[np.nonzero(clusterAssment[:,0].A!=i)[0],1])
+			print 'sseSplit, and notSplit: ', sseSplit, sseNotSplit
+
+			if (sseSplit + sseNotSplit) < lowestSSE:
+				bestCentToSplit = i # i 是用于切分的族
+				bestNewCents = centroidMat
+				bestClustAss = splitClustAss.copy()
+				lowestSSE = sseSplit + sseNotSplit
+
+		# 更新分配结果（bestClustAss中只有两个族的信息，因为每次是二分）
+		bestClustAss[np.nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList)
+		bestClustAss[np.nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
+
+		print 'the bestCentToSplit is: ', bestCentToSplit
+		print 'the len of bestClustAss is: ', len(bestClustAss)
+
+		centList[bestCentToSplit] = bestNewCents[0,:] # 更新被切分的族
+		centList.append(bestNewCents[1,:]) # 添加新的族
+		clusterAssment[np.nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:] = bestClustAss
+
+	# print centList
+	return np.mat(centList), clusterAssment
+
 
 
